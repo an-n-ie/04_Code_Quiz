0b5cb25d14366b5f427f741773e29ec56e5c0350
Annie Hoang <anniehoang.anh@gmail.com>
2022-06-02 00:01:07 -0400

    converted to dynamic html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,7 @@
 var introEL = document.querySelector("#intro")
 var qaContainerEl = document.querySelector("#qa-container")
-var qaContainer2El = document.querySelector("#qa-container2")
-var qaContainer3El = document.querySelector("#qa-container3")
-var inputInitialEl= document.querySelector("#input-initial")
-var resultsEl= document.querySelector("#results")
+var inputInitialEl = document.querySelector("#input-initial")
+var resultsEl= document.querySelector("results")
 var timeEL = document.querySelector("#time")
 var titleEL = document.querySelector("#title")
 var ans1EL = document.querySelector("#ans1")
@@ -11,18 +9,6 @@ var ans2EL = document.querySelector("#ans2")
 var ans3EL = document.querySelector("#ans3")
 var ans4EL = document.querySelector("#ans4")
 var startBtn = document.querySelector("#start-btn")
-var ans11Btn = document.querySelector("#ans1")
-var ans12Btn = document.querySelector("#ans2")
-var ans13Btn = document.querySelector("#ans3")
-var ans14Btn = document.querySelector("#ans4")
-var ans21Btn = document.querySelector("#ans21")
-var ans22Btn = document.querySelector("#ans22")
-var ans23Btn = document.querySelector("#ans23")
-var ans24Btn = document.querySelector("#ans24")
-var ans31Btn = document.querySelector("#ans31")
-var ans32Btn = document.querySelector("#ans32")
-var ans33Btn = document.querySelector("#ans33")
-var ans34Btn = document.querySelector("#ans34")
 var saveBtn = document.querySelector("#save-btn")
 /*
 1. Display start page - title and paragraph start button
@@ -33,7 +19,7 @@ var saveBtn = document.querySelector("#save-btn")
 
 4. Show a dashboard of all the high scores
 */
-var timeRemaining = 45
+var timeRemaining = 60
 var clockId
 
 var question = [{
@@ -64,20 +50,31 @@ function startGame() {
     qaContainerEl.classList.remove("hide")
     introEL.classList.add("hide")
     clockId = setInterval(countdown, 1000)
-    // firstQuestion()
+    firstQuestion()
+}
+function firstQuestion() {
+    titleEL.textContent = question[index].title
+    ans1EL.textContent = question[index].answers[0]
+    ans2EL.textContent = question[index].answers[1]
+    ans3EL.textContent = question[index].answers[2]
+    ans4EL.textContent = question[index].answers[3]
+}
+function nextQuestion() {
+    if(index === question.length){
+        // this is the function being called into action
+         endQuiz()
+    } else {
+        index++
+        firstQuestion()
+    }
 }
 
-function secondQuestion(){
-    qaContainer2El.classList.remove("hide")
-    qaContainerEl.classList.add("hide")
-}
-function thirdQuestion(){
-    qaContainer3El.classList.remove("hide")
-    qaContainer2El.classList.add("hide")
-}
-function enterInit(){
+
+
+
+function endQuiz(){
     inputInitialEl.classList.remove("hide")
-    qaContainer3El.classList.add("hide")
+    qaContainerEl.classList.add("hide")
 }
 function resultsPage(){
     resultsEl.classList.remove("hide")
@@ -85,18 +82,10 @@ function resultsPage(){
 }
 
 
+ans1EL.addEventListener("click", nextQuestion)
+ans2EL.addEventListener("click", nextQuestion)
+ans3EL.addEventListener("click", nextQuestion)
+ans4EL.addEventListener("click", nextQuestion)
 
 startBtn.addEventListener("click", startGame)
-ans11Btn.addEventListener("click", secondQuestion)
-ans12Btn.addEventListener("click", secondQuestion)
-ans13Btn.addEventListener("click", secondQuestion)
-ans14Btn.addEventListener("click", secondQuestion)
-ans21Btn.addEventListener("click", thirdQuestion)
-ans22Btn.addEventListener("click", thirdQuestion)
-ans23Btn.addEventListener("click", thirdQuestion)
-ans24Btn.addEventListener("click", thirdQuestion)
-ans31Btn.addEventListener("click", enterInit)
-ans32Btn.addEventListener("click", enterInit)
-ans33Btn.addEventListener("click", enterInit)
-ans34Btn.addEventListener("click", enterInit)
 saveBtn.addEventListener("click", resultsPage)
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Web_APIs_CodeQuiz
+
+## Description
+
+A quiz application that enables the user to generate a random password based on criteria that they’ve selected. 
+
+## Usage
+The following image shows the application's appearance and functionality:
+
+![The Password Generator](./Assets/image/pwd-gen.png)
+
+## Credits
+
+https://www.w3schools.com/js/default.asp
+
+## URL
+
+GitHub: https://github.com/an-n-ie/03_JavaScript_Password_Generator
+
+Website: https://an-n-ie.github.io/03_JavaScript_Password_Generator/
+
